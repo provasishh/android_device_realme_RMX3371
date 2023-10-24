2ceb1877680c3240a87ac92f9fd3f02bf54ac254
Bruno Martins <bgcngm@gmail.com>
2023-10-24 18:12:15 +0100

    RMX3371: Migrate to restructured sepolicy_vndr
    The new folder structure now looks like this:
    
      device/qcom/sepolicy_vndr
      ├─ legacy-um (meant for sdm845 until sm8350)
      ├─ sm8450
      └─ sm8550
    
    SEPolicy.mk placed in the root automatically redirects devices to the
    proper subfolder.
    
    Change-Id: I4ed1a06af779909d64172da294c4a99a3ae9ba0d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -187,7 +187,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2023-11-05
 
 # SEPolicy
-include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
 
 # Touch
